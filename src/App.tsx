adbbb2b52ebc392ee5ed5883043b6b27f809e85b
M4RC0N3 <marcone@quickup.io>
2023-02-28 01:14:12 -0300

    deploy import resolve

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ const App = () =>{
     setNewData(data);
     
   },[data]);
-
+// eslint-disable-next-line react-hooks/exhaustive-deps
   function Image({ src, alt } : { src:string, alt: string}) {
     const [loaded, setLoaded] = useState(true);
 
